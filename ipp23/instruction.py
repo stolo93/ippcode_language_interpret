97e3d76a1042c8d8b321b5397e08a5fe52f8a6ce
Samuel <xstola03@stud.fit.vutbr.cz>
2023-03-28 13:51:48 +0200

    Argument types

--- ipp23/instruction.py
+++ ipp23/instruction.py
@@ -4,16 +4,112 @@
 @author Samuel Stolarik
 @date 2023-03-22
 """
-import typing
 from abc import ABC, abstractmethod
-import xml.etree.ElementTree
+from enum import Enum
+
+
+class ArgumentType(Enum):
+    VAR = 'var'
+    CONST_VALUE = 'const'
+    LABEL = 'label'
+
+
+class DataType(Enum):
+    NIL = 'nil'
+    INT = 'int'
+    STRING = 'string'
+    BOOL = 'bool'
+
+
+class FrameType(Enum):
+    GF = 'gf'
+    TF = 'tf'
+    LF = 'lf'
+
+
+class Argument(ABC):
+    def __init__(self, arg_type: ArgumentType):
+        self.arg_type = arg_type
+
+    def get_arg_type(self) -> ArgumentType:
+        return self.arg_type
+
+
+class Label(Argument):
+    def __init__(self, label: str):
+        super().__init__(ArgumentType.LABEL)
+        self.label_name = label
+
+
+class Symbol(Argument, ABC):
+    def __init__(self, arg_type: ArgumentType, value=None, value_type: DataType = None):
+        super().__init__(arg_type)
+        self.value = value
+        self.value_type = value_type
+
+    def get_value(self):
+        return self.value
+
+    def get_type(self):
+        return self.type
+
+
+class ConstInt(Symbol):
+    def __init__(self, value: int):
+        super().__init__(ArgumentType.CONST_VALUE, value, DataType.INT)
+
+
+class ConstBool(Symbol):
+    def __init__(self, value: bool):
+        super().__init__(ArgumentType.CONST_VALUE, value, DataType.BOOL)
+
+
+class ConstString(Symbol):
+    def __init__(self, value: str):
+        super().__init__(ArgumentType.CONST_VALUE, value, DataType.STRING)
+
+
+class ConstNil(Symbol):
+    def __init__(self):
+        super().__init__(ArgumentType.CONST_VALUE, 'nil', DataType.NIL)
+
+
+class Variable(Symbol):
+    def __init__(self, name: str, frame: FrameType):
+        super().__init__(ArgumentType.VAR)
+        self.name = name
+        self.frame = frame
+
+    def is_defined(self) -> bool:
+        return self.value is None
+
+    def set_value(self, value, value_type: DataType):
+        self.value = value
+        self.value_type = value_type
+
+    def get_value(self):
+        if self.is_defined():
+            return self.value
+        else:
+            # TODO raise undefined var error
+            pass
+
+    def get_value_type(self) -> DataType:
+        if self.is_defined():
+            return self.value_type
+        else:
+            # TODO raise undefined var error
+            pass
+
+    def get_frame(self) -> FrameType:
+        return self.frame
 
 
 class Instruction(ABC):
     """
     @brief Interface for instructions
     """
-    def __init__(self, opcode: str, order: int, args: list):
+    def __init__(self, opcode: str, order: int, args: list[Argument]):
         """
         @brief Instruction constructor
         @param opcode:
